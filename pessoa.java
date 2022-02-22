97f05edd0a3a5c715061caa7fe2771c03dbd452a
Badwolffn <luckas.a.2016@gmail.com>
2022-02-22 17:33:58 +0000

    isso ai

---
+++ pessoa.java
@@ -0,0 +1,25 @@
+public class pessoa{
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getEndereco(){
+        return this.Endereco;
+    }
+    public void setEndereco(String endereco){
+        this.endereco=endereco;
+    }
+    public String getTelefone(){
+        return this.Telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone=telefone;
+    }
+}
